a217af297d63d17cfb9ebc12909c565eb0de6bc0
Tobias Gruber <tobias_gruber@hotmail.de>
2017-08-04 17:18:25 +0200

    Removed IDENTITY from ChatId column

--- RESTfulChat.DatabaseModel/Tables/ChatUser.sql
+++ RESTfulChat.DatabaseModel/Tables/ChatUser.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ChatUser]
 (
-	[ChatId] INT IDENTITY NOT NULL, 
+	[ChatId] INT NOT NULL, 
     [UserId] INT NOT NULL, 
 	PRIMARY KEY ([UserId], [ChatId]),
     CONSTRAINT [FK_Chat_Chat_User] 
